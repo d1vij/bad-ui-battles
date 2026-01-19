db75b8f13945f22d0b027760c46e7aaebe2a1c77
d1vij <verma.divij@gmail.com>
2026-01-20 01:37:31 +0530

    Add falling behaviour to screw when clicked

--- src/components/Screw/Screw.tsx
+++ src/components/Screw/Screw.tsx
@@ -1,12 +1,28 @@
+import styles from "./screw.module.scss";
+
+import { useRef } from "react";
+
 import screw from "@/assets/skrew.webp";
 import type { ScrewTypes } from "./types";
 
+// PS: Click the circle
 export default function Screw({ className = "" }: ScrewTypes) {
+    const ref = useRef<HTMLImageElement>(null);
+
+    function handleClick() {
+        ref.current?.classList.add(styles.fall);
+        setTimeout(() => {
+            ref.current?.remove();
+        }, 1000);
+    }
+
     return (
         <img
+            onClick={handleClick}
+            ref={ref}
             draggable="false"
             src={screw}
-            className={className + " " + "size-8 origin-center select-none md:size-10"}
+            className={className + " " + "size-8 origin-center cursor-help select-none hover:scale-105 md:size-10"}
         />
     );
 }
---
+++ src/components/Screw/screw.module.d.scss.ts
@@ -0,0 +1,4 @@
+declare const classNames: {
+  readonly fall: "fall";
+};
+export default classNames;
