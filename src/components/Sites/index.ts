f2ff82326a829854008d8b77e9c521ffbf2329fd
d1vij <verma.divij@gmail.com>
2026-01-21 00:13:08 +0530

    Add ReadMe and licence

--- src/components/Sites/index.ts
+++ src/components/Sites/index.ts
@@ -33,54 +33,6 @@ const siteInfo: TSite[] = [
         description: "Six tries at once, Once a day",
         component: Passwordle,
     },
-    {
-        id: generateId(),
-        title: "Birthday Guesser",
-        description: "Guessing your birthday in \nO(logN) time",
-        component: BirthdaySelector,
-    },
-    {
-        id: generateId(),
-        title: "Terms of Service",
-        description: "We ain't take no liability",
-        component: TermsOfServiceLoader,
-    },
-    {
-        id: generateId(),
-        title: "QR Code Scanner",
-        description: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        component: QRCodeScanner,
-    },
-    {
-        id: generateId(),
-        title: "Passwordle",
-        description: "Six tries at once, Once a day",
-        component: Passwordle,
-    },
-    {
-        id: generateId(),
-        title: "Birthday Guesser",
-        description: "Guessing your birthday in \nO(logN) time",
-        component: BirthdaySelector,
-    },
-    {
-        id: generateId(),
-        title: "Terms of Service",
-        description: "We ain't take no liability",
-        component: TermsOfServiceLoader,
-    },
-    {
-        id: generateId(),
-        title: "QR Code Scanner",
-        description: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        component: QRCodeScanner,
-    },
-    {
-        id: generateId(),
-        title: "Passwordle",
-        description: "Six tries at once, Once a day",
-        component: Passwordle,
-    },
 ];
 
 export const SiteIndex = new Map<string, TSite>(siteInfo.map((s) => [generateId(), s]));
