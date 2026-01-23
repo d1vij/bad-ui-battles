1525ab55b9b6d82137ef6e956323fdfe65671742
d1vij <verma.divij@gmail.com>
2026-01-23 21:19:47 +0530

    Stash loader

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,4 +1,4 @@
-import styles from "./styles"
-import type {LoadingProps} from "./types";
+// import styles from "./styles"
+// import type {LoadingProps} from "./types";
 
-export default function Loading({  }: LoadingProps){}
+// export default function Loading({  }: LoadingProps){}
--- src/components/Loading/types.ts
+++ src/components/Loading/types.ts
@@ -1,1 +1,1 @@
-export type LoadingProps = {}
+// export type LoadingProps = {}
