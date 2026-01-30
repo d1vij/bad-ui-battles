d2b0f464eec2de4c2102c974e478777b13518579
d1vij <verma.divij@gmail.com>
2026-01-30 14:00:08 +0530

    Update hooks

--- src/hooks/useVibrate.ts
+++ src/hooks/useVibrate.ts
@@ -1,15 +1,17 @@
 type VibrateFn = (pattern: number | number[]) => boolean;
 
-const vibrator: VibrateFn =
-    "vibrate" in navigator && typeof navigator.vibrate === "function"
-        ? (pattern) => navigator.vibrate(pattern)
-        : () => {
-              console.log("I tried vibrating here (╥﹏╥)");
-              return false;
-          };
 
-// Hook which enables vibration if browser
-// supports vibration otherwise does nothing
+let vibrator: VibrateFn;
+
+if ("vibrate" in navigator && typeof navigator.vibrate === "function") {
+    vibrator = (pattern) => navigator.vibrate(pattern)
+} else {
+    console.log("Your device doesn't support vibrations (╥﹏╥)");
+    vibrator = () => false;
+}
+
+// Hook which allows vibration if browser supports vibration otherwise does nothing
+// Returns true if vibration was successfull, otherwise false
 export function useVibrate() {
     return vibrator;
 }
--- src/hooks/useWindowTitle.ts
+++ src/hooks/useWindowTitle.ts
@@ -2,18 +2,19 @@ import { useEffect, useRef } from "react";
 
 // hook to set the window's title
 export function useWindowTitle(title: string) {
-    const orignalRef = useRef<string>(null);
+    const originalTitle = useRef<string>(null);
+
     useEffect(() => {
         // storing the original title when component gets mounted
-        if (orignalRef.current === null) {
-            orignalRef.current = document.title;
+        if (originalTitle.current === null) {
+            originalTitle.current = document.title;
         }
 
         document.title = title;
         return () => {
             // reset back to original when component unmounts
-            if (orignalRef.current != null) {
-                document.title = orignalRef.current;
+            if (originalTitle.current != null) {
+                document.title = originalTitle.current;
             }
         };
     }, [title]);
